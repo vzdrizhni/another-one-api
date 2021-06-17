075c79696b89d18530f2765706c46a42f21a0696
Danial41 <vzdrizhni@gmail.com>
2021-06-17 16:27:01 +0500

    nasic setup

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+l = Location.create({name: 'Orenburg'})
+l.recordings.create(temp: 32, status: 'sunny')
+l.recordings.create(temp: 25, status: 'cloudy')
+l.recordings.create(temp: 28, status: 'rainy')
+l.recordings.create(temp: 30, status: 'sunny')
+l.recordings.create(temp: 33, status: 'sunny')
